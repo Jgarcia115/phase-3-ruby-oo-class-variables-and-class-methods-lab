f99a36c7ac566af5be27cd3731f6f61ab2d7e7df
Jonathan <jono_95@yahoo.com>
2022-10-20 12:03:15 -0400

    commit tree c4523ded52485c9b88f1262a438e972d566683da

---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+class Song
+    attr_accessor :name, :artist, :genre 
+
+    @@count = 0
+    @@genres = []
+    @@artists= []
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        @@genres.tally
+    end
+
+    def self.artist_count
+        @@artists.tally
+    end
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @@artists.push(artist)
+        @genre = genre
+        @@genres.push(genre)
+        @@count += 1
+    end
+
+    
+
+end
